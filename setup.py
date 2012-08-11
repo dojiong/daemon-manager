b424bb6896b5901d7fa09ad9c1aa860042af7a5f
lo <lodevil@live.cn>
2012-08-11 17:40:33 +0800

    x

--- setup.py
+++ setup.py
@@ -6,12 +6,11 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='daemon-manager',
-      version='0.1',
+      version='0.1.alpha',
       description='Linux Daemon Manager',
       author='Du Jiong/Lodevil',
       author_email='lodevil@live.cn',
       url='https://github.com/lodevil/daemon-manager/',
-      download_url='https://github.com/lodevil/daemon-manager/tarball/master',
       license='BSD',
       py_modules=['dm'],
       entry_points={
