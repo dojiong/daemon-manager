87100ba10786093d6e09c1eb09180988421a9949
lo <lodevil@live.cn>
2012-08-11 17:16:05 +0800

    version

--- setup.py
+++ setup.py
@@ -6,12 +6,11 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='daemon-manager',
-      version='0.1-alpha',
+      version='0.1',
       description='Linux Daemon Manager',
       author='Du Jiong/Lodevil',
       author_email='lodevil@live.cn',
       url='https://github.com/lodevil/daemon-manager/',
-      download_url='https://github.com/lodevil/daemon-manager/zipball/master',
       license='BSD',
       py_modules=['dm'],
       entry_points={
