5c78a3f6a0c2f2f0c9529809d57dabed9e43033f
lo <lodevil@live.cn>
2012-08-16 21:13:02 +0800

    version 0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='daemon-manager',
-      version='0.1.alpha',
+      version='0.1',
       description='Linux Daemon Manager',
       author='Du Jiong/Lodevil',
       author_email='lodevil@live.cn',
